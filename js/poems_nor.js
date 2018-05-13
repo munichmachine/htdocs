bcc2146d6c88459bba48f937a32cefd45629bab8
munichmachine <munichmachine@gmail.com>
2018-05-13 10:04:06 +0200

    Initial commit

---
+++ js/poems_nor.js
@@ -0,0 +1,23 @@
+function loadXMLDoc(dname)
+{
+if (window.XMLHttpRequest)
+  {
+  xhttp=new XMLHttpRequest();
+  }
+else
+  {
+  xhttp=new ActiveXObject("Microsoft.XMLHTTP");
+  }
+xhttp.open("GET",dname,false);
+xhttp.send();
+return xhttp.responseXML;
+} 
+
+function read_nor()
+	{
+		xmlDoc=loadXMLDoc("../xml/poems.xml");
+
+		x=xmlDoc.getElementsByTagName("norwegian")[0]
+		y=x.childNodes[0];
+		document.write(y.nodeValue);
+	}
