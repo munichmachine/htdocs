30c89685d8f3932d8ce8f650967541a348117a0b
munichmachine <munichmachine@gmail.com>
2018-05-15 21:13:20 +0200

    slett visitors

--- visitors/db_connect.php
+++
@@ -1,9 +0,0 @@
-<?php
-//change these values to whatever your own database server requires
-$link = mysql_connect('knivedalen.mysql.domeneshop.no', 'knivedalen', 'wzRge2Fg'); //Connects to the database at "localhost"
-if(!$link) {
-    //halt execution if cannot connect
-    die("Cannot connect to the database!");
-}
-mysql_select_db('knivedalen', $link); //Assuming you have a database named "test" set up
-?>
