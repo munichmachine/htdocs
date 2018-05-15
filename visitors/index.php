88ff0d69052cceee4eb1c4e3a4d0b1237b344594
munichmachine <munichmachine@gmail.com>
2018-05-15 12:02:04 +0200

    visitors

---
+++ visitors/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml"><head>
+<script>
+<?php include("ip_tracker.php"); ?>
+</script>
+</head>
+<body>
+<p>This is page 1</p>
+
+<p>Try navigating between pages to see how the data on the report changes</p>
+
+<p><a href="index2.php">Page 2</a> | <a href="index3.php">Page 3</a></p>
+
+<p><a href="ip_report.php">View Report</a></p>
+</<body>
+</html>
---
+++ visitors/index3.php
@@ -0,0 +1,16 @@
+<html>
+<title>Test av visitors</title>
+<body>
+<?php
+include("ip_tracker.php");
+?>
+
+<p>This is page 3</p>
+
+<p>Try navigating between pages to see how the data on the report changes</p>
+
+<p><a href="index.html">Page 1</a> | <a href="index2.html">Page 2</a></p>
+
+<p><a href="ip_report.php">View Report</a></p>
+</body>
+</html>
